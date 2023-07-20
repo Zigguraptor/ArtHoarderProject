1ec61be1ff8618fcc976180043d12e6218952080
Zigguraptor <cobaltum8@gmail.com>
2023-07-20 19:23:47 +0400

    created HashingAlgorithm

---
+++ ArtHoarderCore/HashAlgs/HashingAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿namespace ArtHoarderCore.HashAlgs;
+
+internal class HashingAlgorithm
+{
+    public string HashName { get; }
+    public Action<double[,], byte[]> ComputeHash;
+
+    public HashingAlgorithm(string hashName, Action<double[,], byte[]> computeHash)
+    {
+        HashName = hashName;
+        ComputeHash = computeHash;
+    }
+}
