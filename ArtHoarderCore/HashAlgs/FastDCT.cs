47bacba90c5fa8b0d4e1917200a731e7b24045a6
Zigguraptor <cobaltum8@gmail.com>
2023-07-20 20:05:51 +0400

    added new dct

---
+++ ArtHoarderCore/HashAlgs/FastDCT.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using ArtHoarderCore.Infrastructure;
+
+namespace ArtHoarderCore.HashAlgs;
+
+public class FastDct : IPerceptualHashAlgorithm
+{
+    [DllImport("PerceptualHashing.dll")]
+    static extern void calculate_dct_hash(double[,] matrix, byte[] output);
+
+    public string HashName { get; } = "FastDCT v1.0";
+
+    public byte[] ComputeHash(double[,] image)
+    {
+        var result = new byte[8];
+        calculate_dct_hash(image, result);
+        return result;
+    }
+}
--- ArtHoarderCore/PerceptualHashing.cs
+++ ArtHoarderCore/PerceptualHashing.cs
@@ -1,75 +1,12 @@
-﻿using ArtHoarderCore.Managers;
-using Mono.Cecil;
+﻿using ArtHoarderCore.HashAlgs;
+using ArtHoarderCore.Infrastructure;
 
 namespace ArtHoarderCore;
 
 internal static class PerceptualHashing
 {
-    private static List<Action<double[,], byte[]>> _algorithms = new();
-    private static Object _syncObj = new();
-
-    static PerceptualHashing()
+    private static IPerceptualHashAlgorithm[] _algorithms =
     {
-        ScanLibsDirectory();
-    }
-
-    private static string _libsDirPath = Constants.PerceptualHashingLibs;
-
-    public static void ScanLibsDirectory()
-    {
-        var libPaths = Directory.GetFiles(_libsDirPath, "*.dll", SearchOption.TopDirectoryOnly);
-        lock (_syncObj)
-        {
-            foreach (var libPath in libPaths)
-            {
-                TargetRuntime? runtimeVer = null;
-                try
-                {
-                    runtimeVer = ModuleDefinition.ReadModule(libPath).Runtime;
-                    if (runtimeVer == TargetRuntime.Net_4_0)
-                    {
-                        RegManagedLib(libPath);
-                        continue;
-                    }
-                }
-                catch
-                {
-                    // ignored
-                }
-
-                RegUnmanagedLib(libPath);
-            }
-        }
-    }
-
-    private static void RegManagedLib(string libPath)
-    {
-        try
-        {
-            var moduleDefinition = ModuleDefinition.ReadModule(libPath);
-            var type = moduleDefinition.Types.SingleOrDefault(t => t.Name == "PerceptualHash");
-            if (type != null && type.IsClass)
-            {
-                var field = type.Fields.SingleOrDefault(f => f.Name == "HashName");
-                if (field != null && field.IsPublic && field.IsStatic && field.IsLiteral &&
-                    field.FieldType.FullName == "System.String")
-                {
-                    var method = type.Methods.SingleOrDefault(m => m.Name == "ComputeHash");
-                    if (method != null)
-                    {
-                        //TODO
-                    }
-                }
-            }
-        }
-        catch (BadImageFormatException e)
-        {
-            Console.WriteLine(e);
-        }
-    }
-
-    private static void RegUnmanagedLib(string libPath)
-    {
-        throw new NotImplementedException();
-    }
+        new FastDct()
+    };
 }
