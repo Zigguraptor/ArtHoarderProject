b642b9e79472356b6ee2b620815552ff585b4749
Zigguraptor <cobaltum8@gmail.com>
2023-07-23 17:58:45 +0400

    added OptionAttribute

---
+++ ArgsParser/Attributes/IArgAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace ArgsParser.Attributes;
+
+public interface IArgAttribute
+{
+    public bool Required { get; set; }
+    public string? GroupName { get; set; }
+}
---
+++ ArgsParser/Attributes/OptionAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace ArgsParser.Attributes;
+
+public class OptionAttribute : Attribute, IArgAttribute
+{
+    private readonly string _name;
+
+    public OptionAttribute(string name, bool required = false)
+    {
+        _name = name;
+        Required = required;
+    }
+
+    public bool Required { get; set; }
+    public string? GroupName { get; set; }
+}
