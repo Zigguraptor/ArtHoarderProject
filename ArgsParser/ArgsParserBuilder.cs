e34ffdf96f979e4a675f4f367da369495e510eff
Zigguraptor <cobaltum8@gmail.com>
2023-07-23 16:50:02 +0400

    created ArgsParserBuilder

---
+++ ArgsParser/ArgsParserBuilder.cs
@@ -0,0 +1,28 @@
+﻿namespace ArgsParser;
+
+public class ArgsParserBuilder
+{
+    private readonly ArgsParserSettings _argsParserSettings;
+    private readonly List<IVerbAction> _verbActions;
+
+    public ArgsParserBuilder() : this(new ArgsParserSettings())
+    {
+    }
+
+    public ArgsParserBuilder(ArgsParserSettings settings)
+    {
+        _argsParserSettings = settings;
+        _verbActions = new List<IVerbAction>();
+    }
+
+    public ArgsParserBuilder AddVerb<T>(Action<T> verbAction)
+    {
+        _verbActions.Add(new VerbAction<T>(verbAction));
+        return this;
+    }
+
+    public ArgsParser Build()
+    {
+        return new ArgsParser(_argsParserSettings, _verbActions);
+    }
+}
