0ece193f3b997751c6069b08f17bd22790bc4c1e
Zigguraptor <cobaltum8@gmail.com>
2023-07-23 16:49:54 +0400

    created ArgsParser

---
+++ ArgsParser/ArgsParser.cs
@@ -0,0 +1,18 @@
+﻿namespace ArgsParser;
+
+public class ArgsParser
+{
+    private readonly ArgsParserSettings _argsParserSettings;
+    private readonly Dictionary<string, IVerbAction> _verbActionsByName;
+
+    public ArgsParser(ArgsParserSettings argsParserSettings, List<IVerbAction> verbActions)
+    {
+        _argsParserSettings = argsParserSettings;
+        _verbActionsByName = new Dictionary<string, IVerbAction>();
+        foreach (var verbAction in verbActions)
+        {
+            var verb = verbAction.Verb;
+            _verbActionsByName.Add(verb.Name, verbAction);
+        }
+    }
+}
