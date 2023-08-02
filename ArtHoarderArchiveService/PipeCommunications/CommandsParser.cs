26b982e45f1dc046f4c79cfa81e5f8190087d524
Zigguraptor <cobaltum8@gmail.com>
2023-08-02 09:47:56 +0400

    updated command building and parsing

--- ArtHoarderArchiveService/PipeCommunications/CommandsParser.cs
+++ ArtHoarderArchiveService/PipeCommunications/CommandsParser.cs
@@ -1,4 +1,5 @@
-﻿using ArgsParser;
+﻿using System.Text.RegularExpressions;
+using ArgsParser;
 using ArtHoarderArchiveService.PipeCommunications.Verbs;
 
 namespace ArtHoarderArchiveService.PipeCommunications;
@@ -18,5 +19,25 @@ public class CommandsParser : ICommandsParser
             .Build();
     }
 
-    public object ParsCommand(string command) => _argsParser.ParseArgs(command.Trim().Split(' '));
+    public ArtHoarderTask ParsCommand(string command)
+    {
+        var strings = SplitCommand(command);
+        var path = strings[0];
+        var args = strings[2..];
+
+        return new ArtHoarderTask(path, _argsParser.ParseArgs(args));
+    }
+
+
+    private static string[] SplitCommand(string s)
+    {
+        if (s.Length > 1)
+        {
+            s = s[1..];
+            s = s[..^1];
+        }
+
+        var args = Regex.Split(s, "(?<=\\\\)*\" \"");
+        return args;
+    }
 }
--- ArtHoarderArchiveService/PipeCommunications/ICommandsParser.cs
+++ ArtHoarderArchiveService/PipeCommunications/ICommandsParser.cs
@@ -2,4 +2,5 @@
 
 public interface ICommandsParser
 {
+    public ArtHoarderTask ParsCommand(string command);
 }
