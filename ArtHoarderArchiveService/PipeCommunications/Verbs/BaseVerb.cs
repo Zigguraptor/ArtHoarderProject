e223da94532ddaa08d2fd523830b315245f2f986
Zigguraptor <cobaltum8@gmail.com>
2023-08-03 02:19:20 +0400

    created BaseVerb

---
+++ ArtHoarderArchiveService/PipeCommunications/Verbs/BaseVerb.cs
@@ -0,0 +1,7 @@
+﻿namespace ArtHoarderArchiveService.PipeCommunications.Verbs;
+
+public abstract class BaseVerb
+{
+    public abstract bool Validate(out List<string> errors);
+    public abstract bool Invoke(CancellationToken cancellationToken);
+}
