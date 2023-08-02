9721834a6443bcb520608bde5403d711374c17cf
Zigguraptor <cobaltum8@gmail.com>
2023-08-02 09:50:09 +0400

    added temp testing code

--- ArtHoarderArchive/Program.cs
+++ ArtHoarderArchive/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using ArtHoarderArchive;
+
+if (args.Length < 1)
+{
+    Console.WriteLine("Enter the command");
+    Console.WriteLine(">");
+
+    var s = Console.ReadLine();
+    if (s != null) args = s.Split(' ');
+}
+
+
+
+var communicator = new NamedPipeCommunicator();
+await communicator.SendCommandAsync(CommandCreator.Create(args));
