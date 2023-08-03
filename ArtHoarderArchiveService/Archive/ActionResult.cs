67f0d4a8c14b2d0afea317c263e9ec333565c334
Zigguraptor <cobaltum8@gmail.com>
2023-08-03 04:59:41 +0400

    created ActionResult

---
+++ ArtHoarderArchiveService/Archive/ActionResult.cs
@@ -0,0 +1,25 @@
+﻿namespace ArtHoarderArchiveService.Archive;
+
+public struct ActionResult
+{
+    public bool IsOk = false;
+    public List<string> Errors = new();
+
+    public ActionResult()
+    {
+    }
+
+    public void AddError(string message) => Errors.Add(message);
+
+    public static ActionResult operator +(ActionResult m1, ActionResult m2)
+    {
+        var errors = new List<string>(m1.Errors);
+        errors.AddRange(m2.Errors);
+
+        return new ActionResult
+        {
+            IsOk = m1.IsOk || m2.IsOk,
+            Errors = errors
+        };
+    }
+}
