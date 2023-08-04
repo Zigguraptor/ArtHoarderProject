578a0605184c03ddaa6b3f794437bd13bb908633
Zigguraptor <cobaltum8@gmail.com>
2023-08-04 18:17:24 +0400

    created cache db

---
+++ ArtHoarderArchiveService/Archive/DAL/CacheDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ArtHoarderArchiveService.Archive.DAL.Entities;
+using ArtHoarderArchiveService.Archive.Managers;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtHoarderArchiveService.Archive.DAL;
+
+internal sealed class CacheDbContext : DbContext
+{
+    public DbSet<ScheduledGalleryUpdateInfo> ScheduledUpdateGalleries { get; set; } = null!;
+    private string DbPath { get; }
+
+    public CacheDbContext(string workDirectory)
+    {
+        DbPath = Path.Combine(workDirectory, Constants.Temp, "Cache"); //TODO literal
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options
+            .UseSqlite($"Data Source={DbPath}");
+}
