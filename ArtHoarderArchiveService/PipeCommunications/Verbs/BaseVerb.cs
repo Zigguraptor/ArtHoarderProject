410b8b14eb0bcfb23326ea18f425fea619140444
Zigguraptor <cobaltum8@gmail.com>
2023-08-06 14:16:22 +0400

    added parallel-run option

--- ArtHoarderArchiveService/PipeCommunications/Verbs/BaseVerb.cs
+++ ArtHoarderArchiveService/PipeCommunications/Verbs/BaseVerb.cs
@@ -1,9 +1,12 @@
-﻿using ArtHoarderArchiveService.Archive;
+﻿using ArgsParser.Attributes;
 
 namespace ArtHoarderArchiveService.PipeCommunications.Verbs;
 
 public abstract class BaseVerb
 {
+    [Option('p', "parallel-run", HelpText = "Run this task as parallel")]
+    public bool IsParallel { get; set; }
+
     public abstract bool Validate(out List<string>? errors);
     public abstract Task Invoke(IMessageWriter messageWriter, string path, CancellationToken cancellationToken);
 }
