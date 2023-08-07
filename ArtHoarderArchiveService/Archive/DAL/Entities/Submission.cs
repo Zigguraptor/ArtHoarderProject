923340a22566bfa3f68b2ad8188a4eb316ab745b
Zigguraptor <cobaltum8@gmail.com>
2023-08-07 05:20:43 +0400

    added public constructor

--- ArtHoarderArchiveService/Archive/DAL/Entities/Submission.cs
+++ ArtHoarderArchiveService/Archive/DAL/Entities/Submission.cs
@@ -6,6 +6,10 @@ namespace ArtHoarderArchiveService.Archive.DAL.Entities;
 
 public class Submission
 {
+    public Submission()
+    {
+    }
+
     public Submission(ParsedSubmission parsedSubmission)
     {
         Uri = parsedSubmission.Uri;
@@ -16,9 +20,9 @@ public class Submission
         PublicationTime = parsedSubmission.PublicationTime;
     }
 
-    [Key] public Uri Uri { get; set; }
+    [Key] public Uri Uri { get; set; } = null!;
 
-    public Uri SourceGalleryUri { get; set; }
+    public Uri SourceGalleryUri { get; set; } = null!;
     [ForeignKey("SourceGalleryUri")] public GalleryProfile SourceGallery { get; set; } = null!;
     public string? Title { get; set; }
     public string? Description { get; set; }
@@ -27,7 +31,7 @@ public class Submission
     public DateTime? PublicationTime { get; set; }
     public DateTime FirstSaveTime { get; set; }
     public DateTime LastUpdateTime { get; set; }
-    
+
     public ICollection<FileMetaInfo> FileMetaInfos { get; set; } = null!;
 
     public void Update(ParsedSubmission parsedSubmission)
