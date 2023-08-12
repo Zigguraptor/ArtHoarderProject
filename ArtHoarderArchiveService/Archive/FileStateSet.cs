c38c6c22819089e5160b6ca602f1446c8c4b371c
Zigguraptor <cobaltum8@gmail.com>
2023-08-12 18:28:16 +0400

    added status command

--- ArtHoarderArchiveService/Archive/FileStateSet.cs
+++ ArtHoarderArchiveService/Archive/FileStateSet.cs
@@ -1,4 +1,6 @@
-﻿namespace ArtHoarderArchiveService.Archive;
+﻿using System.Text;
+
+namespace ArtHoarderArchiveService.Archive;
 
 public class FileStateSet
 {
@@ -13,4 +15,43 @@ public class FileStateSet
     public HashSet<string> UnregisteredFiles { get; }
     public HashSet<string> MissingFiles { get; }
     public HashSet<string> ChangedFiles { get; }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        if (UnregisteredFiles.Count > 0)
+        {
+            sb.Append("Unregistered files:\n");
+            foreach (var fileName in UnregisteredFiles)
+                sb.Append(fileName);
+        }
+        else
+        {
+            sb.Append("No unregistered files.");
+        }
+
+        if (MissingFiles.Count > 0)
+        {
+            sb.Append("missing files:\n");
+            foreach (var fileName in MissingFiles)
+                sb.Append(fileName);
+        }
+        else
+        {
+            sb.Append("No missing files.");
+        }
+
+        if (ChangedFiles.Count > 0)
+        {
+            sb.Append("Changed files:\n");
+            foreach (var fileName in ChangedFiles)
+                sb.Append(fileName);
+        }
+        else
+        {
+            sb.Append("No changed files.");
+        }
+
+        return sb.ToString();
+    }
 }
--- ArtHoarderArchiveService/PipeCommunications/Verbs/StatusVerb.cs
+++ ArtHoarderArchiveService/PipeCommunications/Verbs/StatusVerb.cs
@@ -1,8 +1,22 @@
 ﻿using ArgsParser.Attributes;
+using ArtHoarderArchiveService.Archive;
 
 namespace ArtHoarderArchiveService.PipeCommunications.Verbs;
 
 [Verb("status")]
-public class StatusVerb
+public class StatusVerb : BaseVerb
 {
+    public override bool Validate(out List<string>? errors)
+    {
+        errors = null;
+        return true;
+    }
+
+    public override void Invoke(IMessager messager, ArchiveContextFactory archiveContextFactory, string path,
+        CancellationToken cancellationToken)
+    {
+        using var context = archiveContextFactory.CreateArchiveContext(path);
+        var fileStateSet = context.GetFileStateSet();
+        messager.WriteLine(fileStateSet.ToString());
+    }
 }
