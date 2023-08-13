4d48b8a5751490e81911b6c353f75414987957df
Zigguraptor <cobaltum8@gmail.com>
2023-08-13 15:10:27 +0400

    added null! init

--- ArtHoarderArchiveService/Archive/Parsers/Settings/ParserTypeWSettings.cs
+++ ArtHoarderArchiveService/Archive/Parsers/Settings/ParserTypeWSettings.cs
@@ -3,25 +3,25 @@
 internal class ParserTypeWSettings : ParserSettings
 {
     public int UserNameOrderInProfileLink { get; init; }
-    public string XpathProfileIcon { get; init; }
-    public string XpathProfileName { get; init; }
-    public string XpathProfileCreationDataTime { get; init; }
-    public string XpathProfileStatus { get; init; }
-    public string XpathProfileDescription { get; init; }
-    public string UriIconAttributeName { get; init; }
-    public string XpathSubmissions { get; init; }
-    public string XpathSubmissionPublicationTime { get; init; }
-    public string XpathSubmissionFileSrc { get; init; }
-    public string XpathSubmissionNextFile { get; init; }
-    public string XpathSubmissionFileSrcAttribute { get; init; }
-    public string XpathSubmissionTitle { get; init; }
-    public string XpathSubmissionDescription { get; init; }
-    public string XpathSubmissionTags { get; init; }
-    public string XpathGalleryUri { get; init; }
-    public string XpathNextPageButton { get; init; }
-    public string XpathSubscriptions { get; init; }
-    public string XpathSubscriptionsNextPage { get; init; }
-    public string XpathSubscriptionsNextPageAttribute { get; init; }
-    public string XpathSubscriptionsLinks { get; init; }
-    public string SubmissionNextFileAttribute { get; init; }
+    public string XpathProfileIcon { get; init; } = null!;
+    public string XpathProfileName { get; init; } = null!;
+    public string XpathProfileCreationDataTime { get; init; } = null!;
+    public string XpathProfileStatus { get; init; } = null!;
+    public string XpathProfileDescription { get; init; } = null!;
+    public string UriIconAttributeName { get; init; } = null!;
+    public string XpathSubmissions { get; init; } = null!;
+    public string XpathSubmissionPublicationTime { get; init; } = null!;
+    public string XpathSubmissionFileSrc { get; init; } = null!;
+    public string XpathSubmissionNextFile { get; init; } = null!;
+    public string XpathSubmissionFileSrcAttribute { get; init; } = null!;
+    public string XpathSubmissionTitle { get; init; } = null!;
+    public string XpathSubmissionDescription { get; init; } = null!;
+    public string XpathSubmissionTags { get; init; } = null!;
+    public string XpathGalleryUri { get; init; } = null!;
+    public string XpathNextPageButton { get; init; } = null!;
+    public string XpathSubscriptions { get; init; } = null!;
+    public string XpathSubscriptionsNextPage { get; init; } = null!;
+    public string XpathSubscriptionsNextPageAttribute { get; init; } = null!;
+    public string XpathSubscriptionsLinks { get; init; } = null!;
+    public string SubmissionNextFileAttribute { get; init; } = null!;
 }
