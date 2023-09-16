9659b1560cf34b5d3e305627182198c6a45d254f
Zigguraptor <cobaltum8@gmail.com>
2023-09-16 19:21:09 +0400

    added GetHtmlAsync()

--- ArtHoarderArchiveService/Archive/Networking/IWebDownloader.cs
+++ ArtHoarderArchiveService/Archive/Networking/IWebDownloader.cs
@@ -5,5 +5,6 @@ namespace ArtHoarderArchiveService.Archive.Networking;
 public interface IWebDownloader
 {
     public HtmlDocument GetHtml(Uri uri, CancellationToken cancellationToken);
+    public Task<HtmlDocument> GetHtmlAsync(Uri uri, CancellationToken cancellationToken);
     HttpResponseMessage Get(Uri uri, CancellationToken cancellationToken);
 }
