72179c1c9b9928a2651bda2861c34c468a286e71
Zigguraptor <cobaltum8@gmail.com>
2023-09-16 19:21:26 +0400

    created ExtractedBytes

---
+++ ArtHoarderArchiveService/Archive/Parsers/ExtractedBytes.cs
@@ -0,0 +1,13 @@
+﻿namespace ArtHoarderArchiveService.Archive.Parsers;
+
+public class ExtractedBytes
+{
+    public ExtractedBytes(Uri uri, byte[] bytes)
+    {
+        Uri = uri;
+        Bytes = bytes;
+    }
+
+    public Uri Uri { get; }
+    public byte[] Bytes { get; }
+}
